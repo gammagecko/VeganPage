a5e36448b3f34fbea28fa8a553b0d51001a1722f
Sean McKnelly <smcknel@wgu.edu>
2024-08-30 14:41:31 -0600

    Changed mobile settings.

--- src/main/resources/static/scripts.js
+++ src/main/resources/static/scripts.js
@@ -1,5 +1,6 @@
 function updateLayout() {
-    const isMobile = window.innerWidth <= 768;
+    const isMobile =
+        /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ;
 
     if (isMobile) {
         document.querySelector('.mobile').style.display = 'block';
