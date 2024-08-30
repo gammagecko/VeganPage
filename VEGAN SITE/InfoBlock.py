9f9836791f2129894350b82525acf70ab87f7597
Sean McKnelly <smcknel@wgu.edu>
2024-08-30 10:49:23 -0600

    Added website interface files

--- VEGAN SITE/InfoBlock.py
+++
@@ -1,7 +0,0 @@
-class InfoBlock:
-    def __init__(self, url, title, category, state):
-        self.url = url
-        self.title = title
-        self.category = category
-        self.state = state
-        
--- VEGAN SITE/InfoBlocks.py
+++
@@ -1,8 +0,0 @@
-import InfoBlock
-
-class InfoBlocks:
-    def __init__(self):
-        self.arr = []
-    
-    def add(self, infoblock):
-        self.arr.append(infoblock)
